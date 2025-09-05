2620488712f5c0becd0f21b75887db715732e1c2
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2025-09-05 18:51:21 +0600

    add new deny plan cancelation workflow

---
+++ kindeSrc/environment/workflows/denyPlanCancel/Workflow.ts
@@ -0,0 +1,39 @@
+import {
+  onPlanCancellationRequestEvent,
+  WorkflowSettings,
+  WorkflowTrigger,
+  denyPlanCancellation,
+} from "@kinde/infrastructure"
+
+// The setting for this workflow
+export const workflowSettings: WorkflowSettings = {
+  id: "onUserPlanCancellationRequest",
+  trigger: WorkflowTrigger.UserPlanCancellationRequest,
+  name: "Deny Plan Cancelation",
+  failurePolicy: {
+    action: "stop",
+  },
+  bindings: {
+    "kinde.plan": {},
+  },
+}
+
+// This workflow demonstrates denying a plan cancellation
+
+// The workflow code to be executed when the event is triggered
+export default async function Workflow(event: onPlanCancellationRequestEvent) {
+  const { plans } = event.context.billing
+  const subscription = plans?.[0]
+
+  if (subscription?.subscribed_on) {
+    const start = new Date(subscription.subscribed_on)
+    const minEnd = new Date(start)
+    minEnd.setMonth(minEnd.getMonth() + 3)
+
+    if (new Date() < minEnd) {
+      denyPlanCancellation(
+        `You’re on a 3-month commitment. You can cancel after ${minEnd.toDateString()}.`
+      )
+    }
+  }
+}
